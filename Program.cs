b7c7e44437c4a5bb1e2aa5525cc2e481a84ffa78
Mariam Abdelrahman Youssef <156664021+Mariam0-0@users.noreply.github.com>
2026-02-25 17:32:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace G_NET_9_OOP01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region PART 01
+
+            #region Question 01
+            //======================================================================
+            //Q1: Explain with code example how class and struct behave differently
+            //======================================================================
+
+
+            #endregion
+
+            #region Question 02
+            //======================================================================
+            //Q2 : Explain the difference between public and private access modifiers with an example.
+            //======================================================================
+
+
+            #endregion
+
+            #region Question 03
+            //======================================================================
+            //Q3 : Describe the steps to create and use a class library in Visual Studio.
+            //======================================================================
+
+
+            #endregion
+
+            #region Question 04
+            //======================================================================
+            //Q4 : What is a class library? Why do we use class libraries?
+            //======================================================================
+
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
