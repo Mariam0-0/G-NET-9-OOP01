e5740b808fe1edaafb828cede6b1d905b35519e9
Mariam Abdelrahman Youssef <156664021+Mariam0-0@users.noreply.github.com>
2026-02-25 17:57:30 +0200

    Q01

---
+++ PointClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_9_OOP01
+{
+    internal class PointClass
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public PointClass(int x, int y)
+        {
+            X = x; Y = y;
+        }
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,6 +13,53 @@ namespace G_NET_9_OOP01
             //Q1: Explain with code example how class and struct behave differently
             //======================================================================
 
+            #region 1.Reference Type vs Value Type
+
+            //// class is reference type - struct is value type
+            //PointClass pc1 = new PointClass(1, 2);
+            //PointClass pc2 = pc1;
+
+            //Console.WriteLine($"Point 1 (CLASS): {pc1}");
+            //Console.WriteLine($"Point 2 (CLASS): {pc2}");
+
+
+            //pc2.X = 20;
+
+            //Console.WriteLine($"Point 1 (CLASS): {pc1}");
+            //Console.WriteLine($"Point 2 (CLASS): {pc2}");
+            //// since class is reference type, both pc1 and pc2 change when modifying any of them
+
+            //PointStruct ps1 = new PointStruct(1,2);
+            //PointStruct ps2 = ps1;
+
+            //Console.WriteLine();
+            //Console.WriteLine($"Point 1 (STRUCT): {ps1}");
+            //Console.WriteLine($"Point 2 (STRUCT): {ps2}");
+
+            //ps2.X = 20;
+
+            //Console.WriteLine($"Point 1 (STRUCT): {ps1}");
+            //Console.WriteLine($"Point 2 (STRUCT): {ps2}");
+            //// since struct is value type, ps2 onlu takes a copy of ps1, so ps1 doesn't change when modifying ps2
+            #endregion
+
+            #region 2.Default CTOR
+
+            // CLASS
+            //PointClass pc1 = new PointClass(); // Error because default CTOR no longer exists after creating another CTOR
+            //PointClass pc2 = new PointClass(1, 2); // only the CTOR we created works
+
+            // STRUCT
+            //PointStruct ps1 = new PointStruct();
+            //PointStruct ps2 = new PointStruct(1,2);
+            // both works since default CTOR still exist
+            #endregion
+
+            // 3. Classes support inheritance while structs don't support inheritance
+
+            // 4. Class can be null, but struct cannot (unless nullable)
+
+            // 5. Class is stored in heap while reference is in stack, struct is stored in stack
 
             #endregion
 
